40abf15719dc4b5ad18b7ed94c13ed6799e709df
sevenyjl <50271500+sevenyjl@users.noreply.github.com>
2021-10-12 09:06:31 +0800

    Update MyEncryptedUtils.java

--- MyEncryptedUtils.java
+++ MyEncryptedUtils.java
@@ -41,4 +41,34 @@ public class MyEncryptedUtils {
         }
         return stringBuilder.toString();
     }
+    
+    void enFile() {
+        String dir = "D:\\my\\leetcode\\src\\main\\java";
+        File file = new File(dir);
+        List<File> files = FileUtil.loopFiles(dir);
+        files.forEach(f -> {
+            try {
+                String replace = f.getCanonicalPath().replace(dir, file.getParent() + "\\encrypted") + ".seven";
+                FileUtil.writeString(MyEncryptedUtils.encrypted(FileUtil.readString(f, "UTF-8")), replace, "UTF-8");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+    }
+
+    void deFile() {
+        String dir = "D:\\my\\leetcode\\src\\main\\encrypted";
+        File file = new File(dir);
+        List<File> files = FileUtil.loopFiles(dir);
+        files.forEach(f -> {
+            try {
+                String replace = f.getCanonicalPath().replace(dir, file.getParent() + "\\decode").replace(".seven", "");
+                FileUtil.writeUtf8String(MyEncryptedUtils.decode(FileUtil.readUtf8String(f)), replace);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+    }
 }
